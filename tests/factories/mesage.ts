82a0c936fb64fea4a8073efd04a8a25905c1c15f
yryrgogo <yryrgogo@gmail.com>
2022-10-01 08:21:53 +0900

    :green_heart: add dummy data for test

---
+++ tests/factories/mesage.ts
@@ -0,0 +1,12 @@
+import { Factory } from "fishery";
+import { Timestamp } from "firebase/firestore";
+import { Message } from "~/types/message";
+
+const messageFactory = Factory.define<Message>(({ sequence }) => ({
+  id: sequence.toString(),
+  createdAt: Timestamp.fromDate(new Date()),
+  content: "",
+  senderId: "",
+}));
+
+export { messageFactory };
---
+++ tests/factories/user.ts
@@ -0,0 +1,12 @@
+import { Factory } from "fishery";
+import { Timestamp } from "firebase/firestore";
+import { User } from "~/types/user";
+
+const userFactory = Factory.define<User>(({ sequence }) => ({
+  id: sequence.toString(),
+  createdAt: Timestamp.fromDate(new Date()),
+  name: `テストユーザー${sequence}`,
+  photoUrl: "",
+}));
+
+export { userFactory };
